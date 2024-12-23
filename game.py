848967b798e6c15acc14cad2f33720ad1b885502
A3+++ <jotarokujo0539@gmail.com>
2024-12-23 18:42:49 -0500

    Update game.py

--- game.py
+++ game.py
@@ -31,4 +31,24 @@ while True:
     print(f"Connection from {client_address}")
     clients.append(client_socket)
     threading.Thread(target=handle_client, args=(client_socket,)).start()
-  <script src="https://github.com/eternalxlks/Game/blob/main/client.py"></script>
+    import socket
+import threading
+
+def receive_messages(client_socket):
+    while True:
+        try:
+            message = client_socket.recv(1024).decode("utf-8")
+            print(message)
+        except:
+            print("Disconnected from server.")
+            break
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(("127.0.0.1", 5555))
+
+threading.Thread(target=receive_messages, args=(client,)).start()
+
+print("Connected to server. Type your messages below.")
+while True:
+    message = input()
+    client.send(message.encode("utf-8"))
